69141c67a29cfc5520c8e8dea1940c8ac2fcc40a
Karen <berntsenkaren@hotmail.com>
2017-10-17 14:00:22 +0200

    automatic form generator in html

--- src/main/java/nl/voeding/voedingsmeter/rest/WebsiteEndpoint.java
+++ src/main/java/nl/voeding/voedingsmeter/rest/WebsiteEndpoint.java
@@ -1,5 +1,6 @@
 package nl.voeding.voedingsmeter.rest;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import nl.voeding.voedingsmeter.enums.Eenheid;
 import nl.voeding.voedingsmeter.enums.Productgroep;
+import nl.voeding.voedingsmeter.model.Product;
 
 @RestController
 public class WebsiteEndpoint {
@@ -23,6 +25,12 @@ public class WebsiteEndpoint {
 		System.out.println("getProductgroepen");
 		Productgroep[] productgroep=Productgroep.values();
 		return Arrays.stream(productgroep).map(p->p.toString()).toArray(String[]::new);
+	}
+	
+	@GetMapping("getProductFields")
+	public Object[] getProductFields() {
+		return Arrays.stream(Product.class.getDeclaredFields())
+				.map(i-> i.getName()).toArray();	
 	}
 
 	
