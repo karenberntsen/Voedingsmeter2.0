dff8f19b1b4e2ac92b38c83346776e9b32e13757
Karen <berntsenkaren@hotmail.com>
2017-10-25 11:16:58 +0200

    made function to create a JSON object of producthoeveelheid from input form

--- src/main/resources/static/js/productCalculator.js
+++ src/main/resources/static/js/productCalculator.js
@@ -9,6 +9,16 @@ function x() {
 	getData("getDataFromLogboekdag", maakDropdown,"eenheid");
 }
 
+function voegProductToeAanLogboekdag() {
+	var producthoeveelheid= {};
+	var product = {};
+	product["id"]=document.getElementById("product_id").value;
+	producthoeveelheid["hoeveelheid"]=document.getElementById("hoeveelheid").value;
+	producthoeveelheid["product"]=product;
+	console.log(producthoeveelheid);
+	postData("api", JSON.stringify(producthoeveelheid),function(a){},"");
+}
+
 $(document).ready(function() {
     $( "#productInput" ).autocomplete({
       minLength: 1,
@@ -18,8 +28,9 @@ $(document).ready(function() {
         return false;
       },
       select: function( event, ui ) {
+    	console.log(ui.item);
         $( "#productInput" ).val( ui.item.naam );
-        $( "#naam" ).val( ui.item.naam );
+        $( "#product_id" ).val( ui.item.id );
         $( "#eenheid").val (ui.item.eenheid );
         return false;
       }
